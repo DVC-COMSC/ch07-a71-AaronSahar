d317735e3e243c6527833c2e149128c3bf89508b
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 22:14:12 -0700

    test

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-
-numbers = list(map(int, input().split()))
-
-# ******************************
-# Make your Code
-# ******************************
-
-
-# Use this statement to print out the list element. # Replace the variable 'dist' with your variable
-# print (f'{dist:.2f}', end=' ')
+list = []
+average = 0
+for i in range(10):
+    list.append(int(input("Enter a number")))
+    average += list[i]
+average /= 10
+for i in range(10):
+    list[i] -= average
+    if list[i] < 0:
+        list[i] *= -1
+for i in range(10):
+    print(f'{list[i]:.2f}', end=' ')
+print(average)
